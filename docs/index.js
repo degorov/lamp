52caa90e0a6f1c278e8a9ca3061a393ebbc2806d
Dmitry Egorov <mail@degorov.com>
2020-10-17 23:32:33 +0300

    Effects

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,23 @@
+document.addEventListener('init', (event) => {
+    var pageId = event.target.id;
+    console.log('PAGE LOAD', pageId);
+
+    var iconStatus = event.target.querySelector('.iconStatus');
+
+    if (iconStatus) {
+        if (checkConnection()) {
+            iconStatus.style.color = 'green';
+        } else {
+            iconStatus.style.color = 'red';
+        }
+    }
+
+    if (pageId === 'effects') {
+        console.log('GET EFFECT');
+    } else if (pageId === 'alarm') {
+        window.cmdAlarmSave = document.getElementById('cmdAlarmSave');
+        console.log('GET ALARM');
+    }
 });
 
 function menuOpen() {
@@ -9,5 +29,23 @@ function menuLoad(page) {
     var content = document.getElementById('content');
     var menu = document.getElementById('menu');
     content.load(page).then(menu.close.bind(menu));
+}
+
+function effectLoad(effect, thisOli) {
+    document.querySelectorAll('#effects ons-list-item').forEach((oli) => {
+        if (oli.classList.contains('expanded')) {
+            oli.hideExpansion();
+        }
+        if (oli === thisOli) {
+            oli.querySelector('ons-icon').setAttribute('icon', 'md-check-circle');
+        } else {
+            oli.querySelector('ons-icon').setAttribute('icon', 'md-circle-o');
+        }
+    });
+    console.log('SET EFFECT', effect);
+}
+
+function effectAdjust(thisRange) {
+    console.log('ADJUST', thisRange.value);
 }
 
