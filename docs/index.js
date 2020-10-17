e1913d703265a7ce052b76c7e6f6807998a236e8
Dmitry Egorov <mail@degorov.com>
2020-10-17 23:31:51 +0300

    Main menu

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,13 @@
-const app = {};
-
-ons.ready(() => {
-    app.menuOpen = function () {
-        var menu = document.getElementById('menu');
-        menu.open();
-    };
-
-    app.menuLoad = function (page) {
-        var content = document.getElementById('content');
-        var menu = document.getElementById('menu');
-        content.load(page).then(menu.close.bind(menu));
-    };
 });
+
+function menuOpen() {
+    var menu = document.getElementById('menu');
+    menu.open();
+}
+
+function menuLoad(page) {
+    var content = document.getElementById('content');
+    var menu = document.getElementById('menu');
+    content.load(page).then(menu.close.bind(menu));
+}
+
